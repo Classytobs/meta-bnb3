333bd78d6bc16a637372fddc31a1ec7c06c58e7a
classytobs <adewumitobi20@gmail.com>
2022-11-15 21:23:33 +0100

    adjusted the modal component in navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,9 @@ import Bnbn from '../../Assets/Bnbn.png'
 import {Link} from 'react-router-dom';
 import menu from '../../Assets/menu.png'
 import Modal from 'react-modal';
-import Modal1 from '../Modal/Modal1';
+import meta from '../../Assets/meta.svg'
+import walletconnect from '../../Assets/walletconnect.svg'
+import arrow from '../../Assets/arrow.svg'
 
 export default function Navbar() {
 
@@ -56,7 +58,31 @@ export default function Navbar() {
         </div>
 
             <Modal isOpen={modalIsOpen} className='modal__open'>
-                <Modal1 />
+            <div className='button__modal' >
+                <div className='cwallet'>
+                    <h1>Connect Wallet</h1>
+                    <h2 onClick={handleClickModal}> X </h2>
+                </div>
+
+                <div className='mw'>
+                    <h1 >Choose your preferred wallet:</h1>
+                    <div className='lm'>
+                        <img src={meta} alt="meta" /> 
+                        <div className='lm1'>
+                        <h2>Metamask</h2>
+                        <img src={arrow} alt="arrow" />
+                        </div>
+                    </div>
+                    <div className='lw'>
+                        <img src={walletconnect} alt="walletconnect" /> 
+                        <div className='lw1'>
+                        <h2>WalletConnect</h2>
+                        <img src={arrow} alt="arrow" />
+                        </div>
+                    </div>
+                    
+                </div>
+        </div>
             </Modal>
       
     </div>
